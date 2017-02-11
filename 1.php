3732e794adac7587d8a34b418a40f73c9397cad8
R@J <R@J>
2017-02-11 12:00:14 +0530

    added doc

---
+++ 1.php
@@ -0,0 +1,24 @@
+<?php 
+//default syntax
+echo "helllo baabtra";
+?><br><br><br>
+
+
+<?
+//short open tags
+echo "helllo baabtra";
+?><br><br><br>
+
+<?php //HTML Script Tags  ?>
+
+<script language="php">
+	echo "hello baabtra";
+</script><br><br><br>
+
+<?php //ASP Style Tags ?>
+ 
+ <% 
+ echo "hello babtra";
+ %>
+
+ 
